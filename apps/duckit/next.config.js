34cbd34798e05562a8d71a69bfd3c8d012fb3861
Feriansyah <devferiansyah@gmail.com>
2023-07-03 23:22:15 +0800

    feat: add package tailwindconfig style

--- apps/duckit/next.config.js
+++ apps/duckit/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@acme/core"],
+  transpilePackages: ["@acme/core", "tailwindconfig"],
 }
 
 module.exports = nextConfig
